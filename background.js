20fb848dbe6d2896708e659e56dff25127e09b98
kkavinkk <gk.kavin05@gmail.com>
2025-02-15 23:43:05 -0700

    ADDED COMMENTS TO BACKGROUND.JS

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
+    // Creates a custom right click menu
     chrome.contextMenus.create({
-      id: "selectText",
-      title: "Select Text",
-      contexts: ["selection"]
+      id: "selectText", // identify
+      title: "Fact or Cap", // text in the menu
+      contexts: ["selection"] // will only appear when user selects text
     });
   });
   
   chrome.contextMenus.onClicked.addListener((info, tab) => {
+    // listens to when custom button is clicked
     if (info.menuItemId === "selectText") {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
